5a868cc6709ba4f0e0e0fc8d94c65f8174200bda
Ethan Presberg <ethan@presberg.io>
2023-11-14 19:53:58 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'J.A.I.D.A',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
